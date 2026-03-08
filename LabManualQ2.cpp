5139b99cb7402de74e42241356741f1bc84c15d1
AYUSH BHARDWAJ <ayushbh2019@gmail.com>
2026-03-08 12:32:34 +0530

    Remove exe files

---
+++ LabManualQ2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r,area;
+    cout<<"enter radius";
+    cin>>r;
+    area = (3.14)*r*r;
+    cout<<"the area is:"<<area;
+    return 0;
+
+
+}
