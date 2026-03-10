60585c76d969ad8f9bc7b8fa6392696439c5ef5e
AYUSH BHARDWAJ <ayushbh2019@gmail.com>
2026-03-10 13:37:28 +0530

    butterfly question

---
+++ LabManualQ23.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int m=n-1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        for (int j = 1; j <= 2*(n-i); j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = m; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
