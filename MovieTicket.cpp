be88de80ca077e94a28f2ec65ad69c04b6d7706b
AYUSH BHARDWAJ <ayushbh2019@gmail.com>
2026-03-24 00:06:43 +0530

    Update MovieTicket.cpp

--- MovieTicket.cpp
+++ MovieTicket.cpp
@@ -1,125 +1,68 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
+
 class MovieTicket
 {
-    private:
+private:
     string movieName;
     int seatNumber;
     int ticketPrice;
     bool isBooked;
-    public:
-    MovieTicket()
-    {isBooked = false;
-        ticketPrice=200;
-        seatNumber = 2;
-    }
-    MovieTicket(string krrish, int seat,int cost)
-    {
-            movieName = krrish;
-            seatNumber = seat;
-            ticketPrice = cost;
-    }
-void bookTicket()
-{
-    isBooked = true;
-}
-void cancelTicket()
-{
-    isBooked = false;
-}
-void displayTicketDetails()
-{
-    cout<<"Moviename"<<movieName<<endl;
-    cout<<"\n seatnumber"<< seatNumber<<endl;
-    cout<<"\n ticketPrice"<<ticketPrice<<endl;
-    if (isBooked == true)
-    {
-        cout<<"cancel ticket";
-    }
-    else
-    {
-        cout<<"buy ticket";
-    }
-}
-    
-~MovieTicket()
-{
-    cout<<"\n bill ended";
-}
-int calculatetotalcost(int numberofticket)
-{
-    return ticketPrice*numberofticket;
-}
 
+public:
+    MovieTicket()
+    {
+        movieName = "Krrish";
+        seatNumber = 2;
+        ticketPrice = 200;
+        isBooked = false;
+    }
+
+    MovieTicket(string name, int seat, int cost)
+    {
+        movieName = name;
+        seatNumber = seat;
+        ticketPrice = cost;
+        isBooked = false;
+    }
+
+    void bookTicket()
+    {
+        isBooked = true;
+    }
+
+    void cancelTicket()
+    {
+        isBooked = false;
+    }
+
+    void displayTicketDetails()
+    {
+        cout << "Movie Name: " << movieName << endl;
+        cout << "Seat Number: " << seatNumber << endl;
+        cout << "Ticket Price: " << ticketPrice << endl;
+
+        if (isBooked == true)
+            cout << "Ticket Status: Booked" << endl;
+        else
+            cout << "Ticket Status: Not Booked" << endl;
+    }
+
+    int calculatetotalcost(int numberofticket)
+    {
+        return ticketPrice * numberofticket;
+    }
+
+    ~MovieTicket()
+    {
+        cout << "Bill ended" << endl;
+    }
 };
+
 int main()
 {
-MovieTicket t1;
-t1.displayTicketDetails();
-cout<<"\n total bill "<<t1.calculatetotalcost(5);
-return 0;
+    MovieTicket t1;
+    t1.displayTicketDetails();
+    cout << "Total bill: " << t1.calculatetotalcost(5) << endl;
+    return 0;
 }
-=======
-#include<iostream>
-using namespace std;
-class MovieTicket
-{
-    private:
-    string movieName;
-    int seatNumber;
-    int ticketPrice;
-    bool isBooked;
-    public:
-    MovieTicket()
-    {isBooked = false;
-        ticketPrice=200;
-        seatNumber = 2;
-    }
-    MovieTicket(string krrish, int seat,int cost)
-    {
-            movieName = krrish;
-            seatNumber = seat;
-            ticketPrice = cost;
-    }
-void bookTicket()
-{
-    isBooked = true;
-}
-void cancelTicket()
-{
-    isBooked = false;
-}
-void displayTicketDetails()
-{
-    cout<<"Moviename"<<movieName<<endl;
-    cout<<"\n seatnumber"<< seatNumber<<endl;
-    cout<<"\n ticketPrice"<<ticketPrice<<endl;
-    if (isBooked == true)
-    {
-        cout<<"cancel ticket";
-    }
-    else
-    {
-        cout<<"buy ticket";
-    }
-}
-    
-~MovieTicket()
-{
-    cout<<"\n bill ended";
-}
-int calculatetotalcost(int numberofticket)
-{
-    return ticketPrice*numberofticket;
-}
-
-};
-int main()
-{
-MovieTicket t1;
-t1.displayTicketDetails();
-cout<<"\n total bill "<<t1.calculatetotalcost(5);
-return 0;
-}
->>>>>>> c42ec45e17a73f20e7928ba8e30bfbc54e406d1c
