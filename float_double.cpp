604a51fd2ce17ea411ef3e238012839f3370353d
AYUSH BHARDWAJ <ayushbh2019@gmail.com>
2026-03-24 00:26:17 +0530

    Update float_double.cpp

--- float_double.cpp
+++ float_double.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int sum (int,int);
@@ -32,40 +31,3 @@ int main()
     {
         return e+f;
     }
-
-=======
-#include<iostream>
-using namespace std;
-int sum (int,int);
-float sum (float,float);
-double sum (double,double);
-int main()
-    {
-        int a,b;
-        float c,d;
-        double e,f;
-        cout<<"\n enter two intger";
-        cin>>a>>b;
-        cout<<"\n your sum is "<<sum(a,b);
-        cout<<"\n enter two float";
-        cin>>c>>d;
-        cout<<"\n your sum is "<<sum(c,d);
-        cout<<"\n enter two double";
-        cin>>e>>f;
-        cout<<"\n your sum is "<<sum(e,f);
-        
-    }
-    int sum(int a, int b)
-    {
-        return a+b;
-    }
-    float sum (float c, float d)
-    {
-        return c+d;
-    }
-    double sum(double e, double f)
-    {
-        return e+f;
-    }
-
->>>>>>> c42ec45e17a73f20e7928ba8e30bfbc54e406d1c
