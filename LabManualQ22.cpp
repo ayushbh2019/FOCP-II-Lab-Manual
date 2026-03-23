25860f96981b67ece5e466bb31a3f781c587b072
AYUSH BHARDWAJ <ayushbh2019@gmail.com>
2026-03-23 23:21:38 +0530

    Added LabManualQ22.cpp

--- LabManualQ22.cpp
+++ LabManualQ22.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     int n = 5;
     for (int i = 0; i < 2 * n - 1; i++) {
-        int comp;
-        if (i < n) comp = 2 * (n - i) - 1;
-        else comp = 2 * (i - n + 1) + 1;
-        for (int j = 0; j < comp; j++)
+        int temp;
+        if (i < n) temp = 2 * (n - i) - 1;
+        else temp = 2 * (i - n + 1) + 1;
+        for (int j = 0; j < temp; j++)
             printf(" ");
-        for (int k = 0; k < 2 * n - comp; k++) {
-            if (k == 0 || k == 2 * n - comp - 1)
+        for (int k = 0; k < 2 * n - temp; k++) {
+            if (k == 0 || k == 2 * n - temp - 1)
                 printf("* ");
             else
                 printf("  ");
