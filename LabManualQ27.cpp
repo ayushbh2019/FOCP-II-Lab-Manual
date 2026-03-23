f2a0ca8f7b263f47afb77cc64f9dacf30d8d0122
AYUSH BHARDWAJ <ayushbh2019@gmail.com>
2026-03-23 23:32:02 +0530

    Update LabManualQ27.cpp

--- LabManualQ27.cpp
+++ LabManualQ27.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int main(){
@@ -15,26 +14,3 @@ if (max<price[i])
 max=price[i];
 }
 cout<<"show maximum price"<<max;
-
-
-=======
-#include<iostream>
-using namespace std;
-int main(){
-    int price[10];
-    int max=0,i;
-cout<<"enter the price";
-for(i=0;i<10;i++){
-cin>>price[i];
-}
-max=price[0];
-
-for(i=1;i<10;i++){
-if (max<price[i])
-max=price[i];
-}
-cout<<"show maximum price"<<max;
-
-
->>>>>>> c42ec45e17a73f20e7928ba8e30bfbc54e406d1c
-}
