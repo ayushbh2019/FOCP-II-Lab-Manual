8db991c5a4b7e84ae38b447168b2c5e88085e943
AYUSH BHARDWAJ <ayushbh2019@gmail.com>
2026-03-24 00:14:04 +0530

    Update swapfunction.cpp

--- swapfunction.cpp
+++ swapfunction.cpp
@@ -1,35 +1,25 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
-void swap(int &a,int &b){
-int temp;
-temp = a;
-a = b;
-b = temp;}
 
-int main(){
-    int a,b;
-    cout<<"enter interger values";
-    cin>>a>>b;
-      swap(a,b);
-    cout<<"a "<<a<<"b "<<b;
-
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
 }
-=======
-#include<iostream>
-using namespace std;
-void swap(int &a,int &b){
-int temp;
-temp = a;
-a = b;
-b = temp;}
 
-int main(){
-    int a,b;
-    cout<<"enter interger values";
-    cin>>a>>b;
-      swap(a,b);
-    cout<<"a "<<a<<"b "<<b;
+int main()
+{
+    int a, b;
 
+    cout << "Enter integer values: ";
+    cin >> a >> b;
+
+    swap(a, b);
+
+    cout << "After swapping:\n";
+    cout << "a = " << a << " b = " << b;
+
+    return 0;
 }
->>>>>>> c42ec45e17a73f20e7928ba8e30bfbc54e406d1c
