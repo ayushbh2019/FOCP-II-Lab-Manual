eb1b24dde1159ddc7500351e2aa519ac92719cac
AYUSH BHARDWAJ <ayushbh2019@gmail.com>
2026-03-24 00:04:38 +0530

    Add files via upload

---
+++ rectangle.cpp
@@ -0,0 +1,87 @@
+<<<<<<< HEAD
+#include<iostream>
+using namespace std;
+class rectangle
+{
+    float width;
+    float length;
+public:
+void inputDimensions();
+float calculateArea();
+float calculatePerimeter();
+void displayresult();
+rectangle(){
+length=0;width=0;}
+rectangle(int l,int w){
+    length=l;width=w;
+}
+};
+void rectangle::inputDimensions(){
+    cout<<"enter the dimensions (width,length)";
+    cin>>width>>length;
+
+}
+float rectangle::calculateArea(){
+    return (width*length);
+}
+float rectangle::calculatePerimeter(){
+    return 2*(width+length);
+}
+void rectangle::displayresult(){
+    cout<<"\n length"<<length<<"\nwidth"<<width;
+    cout<<"\n area"<<calculateArea();
+    cout<<"\n perimeter"<<calculatePerimeter();
+}
+
+int main(){
+    rectangle r1,r2(23,78);
+    //r1.inputDimensions();
+    //cout<<r1.calculateArea();
+    //r1.calculatePerimeter();
+    r1.displayresult();r2.displayresult();
+    return 0;
+}
+=======
+#include<iostream>
+using namespace std;
+class rectangle
+{
+    float width;
+    float length;
+public:
+void inputDimensions();
+float calculateArea();
+float calculatePerimeter();
+void displayresult();
+rectangle(){
+length=0;width=0;}
+rectangle(int l,int w){
+    length=l;width=w;
+}
+};
+void rectangle::inputDimensions(){
+    cout<<"enter the dimensions (width,length)";
+    cin>>width>>length;
+
+}
+float rectangle::calculateArea(){
+    return (width*length);
+}
+float rectangle::calculatePerimeter(){
+    return 2*(width+length);
+}
+void rectangle::displayresult(){
+    cout<<"\n length"<<length<<"\nwidth"<<width;
+    cout<<"\n area"<<calculateArea();
+    cout<<"\n perimeter"<<calculatePerimeter();
+}
+
+int main(){
+    rectangle r1,r2(23,78);
+    //r1.inputDimensions();
+    //cout<<r1.calculateArea();
+    //r1.calculatePerimeter();
+    r1.displayresult();r2.displayresult();
+    return 0;
+}
+>>>>>>> c42ec45e17a73f20e7928ba8e30bfbc54e406d1c
---
+++ smartlight.cpp
@@ -0,0 +1,154 @@
+<<<<<<< HEAD
+#include <iostream>
+using namespace std;
+
+class SmartLight {
+private:
+    string brand;
+    int brightness;   // 0-100
+    bool isOn;
+
+public:
+    // Default constructor
+    SmartLight() {
+        brand = "NoBrand";
+        brightness = 50;
+        isOn = false;
+    }
+
+    // Parameterised constructor
+    SmartLight(string b, int br, bool on) {
+        brand = b;
+        brightness = br;
+        isOn = on;
+    }
+
+    void turnOn() {
+        isOn = true;
+    }
+
+    void turnOff() {
+        isOn = false;
+    }
+
+    void increaseBrightness() {
+        brightness = brightness + 10;
+    }
+
+    void decreaseBrightness() {
+        brightness = brightness - 10;
+    }
+
+    void displayStatus() {
+        cout << "Brand: " << brand << endl;
+        cout << "Brightness: " << brightness << endl;
+        cout << "Light is: ";
+        if (isOn == true)
+            cout << "ON";
+        else
+            cout << "OFF";
+        cout << endl << endl;
+    }
+
+    // Destructor
+    ~SmartLight() {
+        cout << "Object destroyed" << endl;
+    }
+};
+
+int main() {
+
+    SmartLight light1;  // default object
+    SmartLight light2("Philips", 80, true);  // parameterised object
+light1.displayStatus();
+light2.displayStatus();
+    // Testing light1
+    light1.turnOn();
+    light1.increaseBrightness();
+    light2.decreaseBrightness();
+    light1.displayStatus();
+
+    // Testing light2
+    light2.decreaseBrightness();
+    light2.turnOff();
+    light2.displayStatus();
+
+    return 0;
+=======
+#include <iostream>
+using namespace std;
+
+class SmartLight {
+private:
+    string brand;
+    int brightness;   // 0-100
+    bool isOn;
+
+public:
+    // Default constructor
+    SmartLight() {
+        brand = "NoBrand";
+        brightness = 50;
+        isOn = false;
+    }
+
+    // Parameterised constructor
+    SmartLight(string b, int br, bool on) {
+        brand = b;
+        brightness = br;
+        isOn = on;
+    }
+
+    void turnOn() {
+        isOn = true;
+    }
+
+    void turnOff() {
+        isOn = false;
+    }
+
+    void increaseBrightness() {
+        brightness = brightness + 10;
+    }
+
+    void decreaseBrightness() {
+        brightness = brightness - 10;
+    }
+
+    void displayStatus() {
+        cout << "Brand: " << brand << endl;
+        cout << "Brightness: " << brightness << endl;
+        cout << "Light is: ";
+        if (isOn == true)
+            cout << "ON";
+        else
+            cout << "OFF";
+        cout << endl << endl;
+    }
+
+    // Destructor
+    ~SmartLight() {
+        cout << "Object destroyed" << endl;
+    }
+};
+
+int main() {
+
+    SmartLight light1;  // default object
+    SmartLight light2("Philips", 80, true);  // parameterised object
+light1.displayStatus();
+light2.displayStatus();
+    // Testing light1
+    light1.turnOn();
+    light1.increaseBrightness();
+    light2.decreaseBrightness();
+    light1.displayStatus();
+
+    // Testing light2
+    light2.decreaseBrightness();
+    light2.turnOff();
+    light2.displayStatus();
+
+    return 0;
+>>>>>>> c42ec45e17a73f20e7928ba8e30bfbc54e406d1c
+}
